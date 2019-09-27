eac050f49612da502cd3afb28bff21a30f5c0c33
urbanware-org <github@urbanware.org>
2019-09-27 23:07:29 +0200

    Reduce code (move to core modules)

--- inhibit.sh
+++ inhibit.sh
@@ -2,6 +2,7 @@
 
 # ============================================================================
 # Inhibit - Prevent accidental execution of certain commands
+# Main script
 # Copyright (C) 2019 by Ralf Kilian
 # Distributed under the MIT License (https://opensource.org/licenses/MIT)
 #
@@ -9,70 +10,41 @@
 # GitLab: https://gitlab.com/urbanware-org/inhibit
 # ============================================================================
 
-version="1.1.0"
-
 script_dir=$(dirname $(readlink -f $0))
-config_file="$dirname/inhibit.conf"
+script_file=$(basename "$0")
+source ${script_dir}/core/common.sh
+source ${script_dir}/core/systemctl.sh
 
-# Do not change any of the values below! See the 'inhibit.conf' file for
-# configuration options.
-use_colors=1
-use_random=0
-random_count=8
-random_upper=0
-
+config_file="$script_dir/inhibit.conf"
 if [ -f "$config_file" ]; then
     source $config_file
 fi
+apply_config
 
-command=$1
-if [ -z "$command" ]; then
-    echo "usage: inhibit.sh COMMAND"
-    exit 0
-fi
-
-if [ $use_colors -eq 1 ]; then
-    cn="\e[0m"      # none (default color)
-    cc="\e[1;36m"   # cyan
-    cg="\e[1;32m"   # green
-    cr="\e[1;31m"   # red
-    cy="\e[1;33m"   # yellow
+if [ -z "$1" ] || [ "$1" = "--help" ]; then
+    usage
+elif [ "$1" = "--version" ]; then
+    print_version
 else
-    qt="'"
+    inhibit_command="$@"
 fi
 
 if [ $use_random -eq 1 ]; then
-    if [ $random_count -gt 32 ]; then
-        random_count=32
-    elif [ $random_count -lt 4 ]; then
-        random_count=4
-    fi
-    if [ $random_upper -eq 1 ]; then
-        confirm_string=$(uuidgen | md5sum | base64 | head -c$random_count)
-    else
-        confirm_string=$(uuidgen | md5sum | head -c$random_count)
-    fi
+    random_string
     confirm_type="Sequence"
 else
     confirm_string=$(hostname -s)
     confirm_type="Hostname"
 fi
 
-echo
-echo -e "${cy}Warning!$cn The ${qt}${cc}$command${cn}${qt} command has been"\
-        "${cr}inhibited${cn}!"
-echo
-echo "In order to proceed you have to confirm the process."
-echo
-echo -e "${cc}$confirm_type:${cn} $confirm_string"
-echo -e "${cc}Confirm:${cn}  \c"
-read user_input
-echo
-if [ "$confirm_string" = "$user_input" ]; then
-    echo -e "Confirmation ${cg}successful${cn}. Proceeding."
-    $@
+if [[ $inhibit_command == *inhibit.sh* ]]; then
+    usage "Inhibiting the script itself? This does not make any sense"
+fi
+
+if [[ $inhibit_command == *systemctl* ]]; then
+    inhibit_service_control
 else
-    echo -e "Confirmation ${cr}failed${cn}. Process canceled.\n"
+    inhibit_command_execution
 fi
 
 # EOF
