a5714a9c376fa2064b2d103508606d906bb3375a
urbanware-org <github@urbanware.org>
2024-03-04 22:04:11 +0100

    Move certain code into shell core script

--- core/common.sh
+++ core/common.sh
@@ -12,15 +12,6 @@
 
 version="1.2.0"
 
-header_default="
-####  ##    ##  ##    ##  ####  #######   ####  ########  ########  #######
- ##   ###   ##  ##    ##   ##   ##    ##   ##      ##     ##        ##    ##
- ##   ####  ##  ##    ##   ##   ##    ##   ##      ##     ##        ##    ##
- ##   ## ## ##  ########   ##   #######    ##      ##     ######    ##    ##
- ##   ##  ####  ##    ##   ##   ##    ##   ##      ##     ##        ##    ##
- ##   ##   ###  ##    ##   ##   ##    ##   ##      ##     ##        ##    ##
-####  ##    ##  ##    ##  ####  #######   ####     ##     ########  #######"
-
 apply_config() {
     if [ $config_missing -eq 1 ]; then
         # Defaults for the configuration options in case the config file is
@@ -78,53 +69,6 @@ apply_config() {
         cy="\e[1;33m"   # yellow
     else
         use_colors=0
-    fi
-}
-
-confirm_string() {
-    echo
-    echo -e "${cc}${confirm_type}:${cn} ${confirm_string}"
-    echo -e "${cc}Confirm:${cn}  \c"
-    read user_input
-    echo
-    if [ "$confirm_string" = "$user_input" ]; then
-        success=1
-    fi
-}
-
-inhibit_command_execution() {
-    if [ $show_header -eq 1 ]; then
-        if [ -z "$header" ]; then
-            echo -e "${header_default}\e[0m"
-        else
-            echo -e "${header}\e[0m"
-        fi
-    fi
-    echo
-    echo -e "${cy}Warning!$cn The '${cc}${inhibit_command}${cn}'"\
-            "command has been ${cr}inhibited${cn}!"
-    echo
-    echo "In order to proceed you have to confirm the process."
-
-    success=0
-    tries=$max_tries
-    while [ $tries -gt 0 ]; do
-        confirm_string
-        if [ $success -eq 1 ]; then
-            break
-        else
-            tries=$(( tries - 1 ))
-            if [ $tries -gt 0 ]; then
-                echo -e "Confirmation ${cr}failed${cn}. Please retry."
-            fi
-        fi
-    done
-
-    if [ $success -eq 1 ]; then
-        echo -e "Confirmation ${cg}successful${cn}. Proceeding.\n"
-        $inhibit_command  # execute inhibited command
-    else
-        echo -e "Confirmation ${cr}failed${cn}. Process ${cr}canceled${cn}.\n"
     fi
 }
 
